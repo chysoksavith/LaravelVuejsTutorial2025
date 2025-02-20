77170e4702ee6fff34f520874123162293263408
chysoksavith <120388241+chysoksavith@users.noreply.github.com>
2025-02-20 09:44:04 +0700

    set up pinia persistedstate pluugin

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,8 +9,11 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
 const app = createApp(App);
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 app.use(router);
 app.use(plugin, defaultConfig(config));
 app.use(pinia);
--- frontend/src/store/auth.ts
+++ frontend/src/store/auth.ts
@@ -6,72 +6,81 @@ import { AxiosError } from "axios";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-export const useAuthStore = defineStore("auth", () => {
-  const user = ref<User | null>(null);
-  const isLoggedIn = ref<boolean>(false);
+export const useAuthStore = defineStore(
+  "auth",
+  () => {
+    const user = ref<User | null>(null);
+    const isLoggedIn = ref<boolean>(false);
 
-  //   register
-  const register = async (payload: RegisterForm, node?: FormKitNode) => {
-    await axiosInstance.get("/sanctum/csrf-cookie", {
-      baseURL: "http://localhost:8000",
-    });
+    //   register
+    const register = async (payload: RegisterForm, node?: FormKitNode) => {
+      await axiosInstance.get("/sanctum/csrf-cookie", {
+        baseURL: "http://localhost:8000",
+      });
 
-    try {
-      await axiosInstance.post("/register", payload);
-      await getUser();
-      router.push("/dashboard");
-    } catch (e) {
-      if (e instanceof AxiosError && e.response?.status === 422) {
-        node?.setErrors([], e.response?.data.errors);
+      try {
+        await axiosInstance.post("/register", payload);
+        await getUser();
+        router.push("/dashboard");
+      } catch (e) {
+        if (e instanceof AxiosError && e.response?.status === 422) {
+          node?.setErrors([], e.response?.data.errors);
+        }
       }
-    }
-  };
-  //   login
-  const login = async (payload: LoginForm, node?: FormKitNode) => {
-    // Ensure CSRF cookie for Laravel Sanctum
-    await axiosInstance.get("/sanctum/csrf-cookie", {
-      baseURL: "http://localhost:8000",
-    });
+    };
+    //   login
+    const login = async (payload: LoginForm, node?: FormKitNode) => {
+      // Ensure CSRF cookie for Laravel Sanctum
+      await axiosInstance.get("/sanctum/csrf-cookie", {
+        baseURL: "http://localhost:8000",
+      });
 
-    try {
-      // Send login request
-      await axiosInstance.post("/login", payload);
-      await getUser();
-      router.push("/dashboard");
-    } catch (e: any) {
-      if (e instanceof AxiosError && e.response?.status === 422) {
-        node?.setErrors([], e.response?.data.errors);
+      try {
+        // Send login request
+        await axiosInstance.post("/login", payload);
+        await getUser();
+        router.push("/dashboard");
+      } catch (e: any) {
+        if (e instanceof AxiosError && e.response?.status === 422) {
+          node?.setErrors([], e.response?.data.errors);
+        }
       }
-    }
-  };
-  // getUser
-  const getUser = async () => {
-    try {
-      const response = await axiosInstance.get("/user");
-      user.value = response.data;
-      isLoggedIn.value = true;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  // logout
-  const logout = async () => {
-    try {
-      await axiosInstance.post("/logout");
-      user.value = null;
-      isLoggedIn.value = false;
-      router.push("/login");
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    };
+    // getUser
+    const getUser = async () => {
+      try {
+        const response = await axiosInstance.get("/user");
+        user.value = response.data;
+        isLoggedIn.value = true;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    // logout
+    const logout = async () => {
+      try {
+        await axiosInstance.post("/logout");
+        user.value = null;
+        isLoggedIn.value = false;
+        router.push("/login");
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  return {
-    user,
-    isLoggedIn,
-    register,
-    login,
-    getUser,
-    logout,
-  };
-});
+    return {
+      user,
+      isLoggedIn,
+      register,
+      login,
+      getUser,
+      logout,
+    };
+  },
+  {
+    persist: {
+      storage: sessionStorage,
+      pick: ["user", "isLoggedIn"],
+    },
+  }
+);
